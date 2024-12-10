0dc04d40e3d16a1013c7da01f095dfbd02b64f96
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2024-12-10 23:39:40 +0600

    src.java

---
+++ src.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bloodmanagement;
+
+/**
+ *
+ * @author User
+ */
+public class BloodManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        startJFrame l = new startJFrame();
+        l.show();
+        
+    }
+    
+}
